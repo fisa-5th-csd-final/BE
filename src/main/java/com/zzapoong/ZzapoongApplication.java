c8a00c7aeb9745ccb0f6c37e026003be2bb3348f
Bellmin <whdals011203@naver.com>
2025-10-15 11:17:42 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/zzapoong/ZzapoongApplication.java
@@ -0,0 +1,13 @@
+package com.zzapoong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzapoongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZzapoongApplication.class, args);
+    }
+
+}
